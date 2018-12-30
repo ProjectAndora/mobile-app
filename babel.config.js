3298d8ba317d579c968a7345c73f233383ac1aca
thorny <35000252+happy-thorny@users.noreply.github.com>
2018-12-31 02:55:52 +0700

    Babel TS presets and plugins removed (is suppored out of box)

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,6 @@
 module.exports = function(api) {
   api.cache(true)
   return {
-    presets: [
-      'babel-preset-expo',
-      '@babel/preset-typescript',
-    ],
-    plugins: [
-      '@babel/proposal-class-properties',
-      '@babel/proposal-object-rest-spread',
-    ],
+    presets: ['babel-preset-expo'],
   }
 }
