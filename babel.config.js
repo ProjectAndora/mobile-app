af09bdac7902dcda030def944a1ef9362dd1be22
thorny <35000252+happy-thorny@users.noreply.github.com>
2018-12-31 02:45:07 +0700

    Typescript for babel

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,13 @@
 module.exports = function(api) {
   api.cache(true)
   return {
-    presets: ['babel-preset-expo'],
+    presets: [
+      'babel-preset-expo',
+      '@babel/preset-typescript',
+    ],
+    plugins: [
+      '@babel/proposal-class-properties',
+      '@babel/proposal-object-rest-spread',
+    ],
   }
 }
