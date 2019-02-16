7e97d183d57d19c00f0ed5ed1f543e87e7207c0f
thorny <35000252+happy-thorny@users.noreply.github.com>
2019-02-16 22:09:46 +0700

    Blacklist config fixed

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,9 @@
-const blacklist = require('metro-config/src/defaults/blacklist')
 const env = require('./env')
 
 module.exports = function(api) {
   api.cache(true)
   return {
     presets: [env === 'expo' ? 'babel-preset-expo' : 'module:metro-react-native-babel-preset'],
-    resolver: {
-    blacklistRE: blacklist([/ios\/.*/, /android\/.*/]),
-    },
     plugins: [
       [
         require.resolve('babel-plugin-module-resolver'),
