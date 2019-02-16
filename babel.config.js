635c43e5d909653a582db248beaab6e785243c1b
thorny <35000252+happy-thorny@users.noreply.github.com>
2019-02-16 19:19:11 +0700

    babel.config.js depends on env; ios and android dirs blacklisted

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,13 @@
+const blacklist = require('metro-config/src/defaults/blacklist')
+const env = require('./env')
+
 module.exports = function(api) {
   api.cache(true)
   return {
-    presets: ['babel-preset-expo'],
+    presets: [env === 'expo' ? 'babel-preset-expo' : 'module:metro-react-native-babel-preset'],
+    resolver: {
+    blacklistRE: blacklist([/ios\/.*/, /android\/.*/]),
+    },
     plugins: [
       [
         require.resolve('babel-plugin-module-resolver'),
