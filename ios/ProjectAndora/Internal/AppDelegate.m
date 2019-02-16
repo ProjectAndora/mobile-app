b7f5f54575315d6d4026e8ac6dd9f57568a3a426
thorny <35000252+happy-thorny@users.noreply.github.com>
2019-02-16 22:12:14 +0700

    IOS app cool and good 🎊

--- ios/ProjectAndora/Internal/AppDelegate.m
+++ ios/ProjectAndora/Internal/AppDelegate.m
@@ -1,4 +1,5 @@
 #import "AppDelegate.h"
+#import <React/RCTRootView.h>
 
 @interface AppDelegate ()
 
@@ -7,12 +8,22 @@
 
 @implementation AppDelegate
 
-
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   self.window.backgroundColor = [UIColor whiteColor];
-  self.window.rootViewController = [[UIViewController alloc] init];
+  
+  NSURL *jsCodeLocation;
+#ifdef DEBUG
+  jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/app/index.bundle?platform=ios"];
+#else
+  jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+#endif
+  
+  UIViewController *vc = [[UIViewController alloc] init];
+  vc.view = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation moduleName:@"App" initialProperties:nil launchOptions:launchOptions];
+  
+  self.window.rootViewController = vc;
   [self.window makeKeyAndVisible];
   
   return YES;
